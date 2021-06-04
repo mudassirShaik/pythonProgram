7080e6f3c0b554d96727a4c246695f6676d57836
mudassirShaik <75924368+mudassirShaik@users.noreply.github.com>
2021-06-04 10:57:34 +0530

    Add files via upload

---
+++ cowin_1.py
@@ -0,0 +1,40 @@
+#Libraries Import
+import requests
+from datetime import datetime
+import time
+
+#Project Init
+now = datetime.now()
+today_date = now.strftime("%d-%m-%Y")
+pinCode = [ "500037","500018", "500043"]
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+for pins in pinCode:
+    URL = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}'.format(pins, today_date)
+    
+#check Avalibility
+def findAvailability():
+    result = requests.get(URL, headers=header)
+    response_json = result.json()
+    data = response_json["sessions"]
+    for each in data:
+        if((each["available_capacity"] > 0) & (each["min_age_limit"] == 18)):
+            message = "Name: {}, \nAddress: {}, \nType: {}, \nAvailability: {}, \nBrand: {}, \nAge: {}\n\n".format(
+                each["name"], each["address"], each["fee_type"],
+                each["available_capacity_dose1"], each["vaccine"],
+                each["min_age_limit"])
+            #sendMsg(message)
+            print(message)
+
+#Telegram Bot
+api_URL = "https://api.telegram.org/bot1897607188:AAGKQahxETtNKJizNxfzSN-RmgCJ9SeoL5k/sendMessage?chat_id=-550067442&text="
+
+def sendMsg(message):
+    send_msg = api_URL + message
+    response = requests.get(send_msg)
+    return response.json()
+
+#Sleep
+while(findAvailability() != True):
+    time.sleep(60)
+    findAvailability()
